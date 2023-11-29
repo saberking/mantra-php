5e344eef823f7128952fd70f767c597dfa9f8a82
Saber <saberking321@protonmail.com>
2023-11-29 22:49:57 +0000

    old-boots

--- html/php/components/scripts.php
+++ html/php/components/scripts.php
@@ -1,6 +1,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="/js/jquery.easing.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+<!--<script src="/js/bootstrap.min.js"></script>-->
 <script src="/js/jquery.parallax-1.1.3.js"></script>
 <script src="/js/jquery.fitvids.js"></script>
 <script src="/js/jquery.magnific-popup.min.js"></script>
