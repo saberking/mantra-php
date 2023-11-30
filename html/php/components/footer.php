3cc7d48729e98070130795ea7535d714d7351f05
Saber <saberking321@protonmail.com>
2023-11-30 22:34:50 +0000

    footer

--- html/php/components/footer.php
+++ html/php/components/footer.php
@@ -9,7 +9,7 @@
             <div class="image-full mb-20">
 
                 <a href="#">
-                    <img src="http://patrykowczarek.com/demo/vistous/img/logo-light-small.png" alt="Logo">
+                    <img src="/img/logo-light-small.png" alt="Logo">
                 </a>
 
             </div>
@@ -32,98 +32,10 @@
 
         </div> <!-- end column -->
 
-        <div class="col-md-3 col-sm-6 col-xs-12">
+ 
 
-            <div class="widget mb-20">
-
-                <h6 class="widget-title">Sign up for Newsletter</h6>
-
-                <div class="widget-subscribe">
-
-                    <form action="php/newsletter-subscribe.php?action=signup" method="post" class="form-alt-2 newsletter-form" novalidate>
-
-                        <div class="form-group">
-
-                            <div class="input-group">
-
-                                <input type="email" class="form-control" name="newsletterEmail" id="newsletterEmail" placeholder="Email">
-
-                                <span class="input-group-btn">
-                                    <button type="submit" class="btn btn-bg-dark">
-                                        <i class="fa fa-send mr-0"></i>
-                                    </button>
-                                </span>
-
-                            </div> <!-- end .input-group -->
-
-                        </div> <!-- end .form-group -->
-
-                    </form>
-
-                </div> <!-- end .widget-subscribe -->
-
-            </div> <!-- end .widget -->
-
-            <div class="widget">
-
-                <div class="widget-text">
-
-                    <ul class="tags">
-                        <li><a href="#">Branding</a></li>
-                        <li><a href="#">Technology</a></li>
-                        <li><a href="#">News</a></li>
-                        <li><a href="#">Design</a></li>
-                        <li><a href="#">Web</a></li>
-                    </ul> <!-- end .tags -->
-
-                </div> <!-- widget-text -->
-
-            </div> <!-- end .widget -->					
-
-        </div> <!-- end column -->
-
-        <div class="col-md-3 col-sm-6 col-xs-12">
-
-            <div class="widget">
-
-                <h6 class="widget-title">Useful Links</h6>
-
-                <div class="widget-nav">
-
-                    <ul class="nav">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Demos</a></li>
-                        <li><a href="#">Pages</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Buy</a></li>
-                        <li><a href="#">Social</a></li>
-                    </ul> <!-- end .nav -->
-
-                </div> <!-- end .widget-nav -->
-
-            </div> <!-- end .widget -->
-
-            
-
-        </div> <!-- end column -->
-
-        <div class="col-md-3 col-sm-6 col-xs-12">
-
-            <div class="widget">
-
-                <h5 class="widget-title">Twitter Feed</h5>
-
-                <div class="widget-twitter">
-
-                    <ul class="twitter-feed" data-plugin-options='{"username": "envato", "count": "1"}'>
-                        <li></li>
-                    </ul> <!-- end .twitter-feed -->
-
-                </div> <!-- end .widget-twitter -->
-
-            </div> <!-- end .widget -->
-
-        </div> <!-- end column -->
+      
+ 
 
     </div> <!-- end .row -->
 
@@ -143,23 +55,6 @@
 
             </div>
 
-            <div class="col-md-6 col-sm-12">
-
-                <nav class="align-right">
-
-                    <ul class="nav nav-horiz">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Pages</a></li>
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">Work</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Shop</a></li>
-                        <li><a href="#">Elements</a></li>
-                    </ul> <!-- end .nav -->
-
-                </nav>
-
-            </div>
 
     </div> <!-- end .container -->
 
