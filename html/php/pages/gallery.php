bc17fc270416057de30e2087dda93500078e6924
Saber <saberking321@protonmail.com>
2023-12-01 03:32:07 +0000

    old-php

--- html/php/pages/gallery.php
+++ html/php/pages/gallery.php
@@ -43,7 +43,7 @@
 				<?php 
 				$files = scandir($root_dir . '/img/mantra/');
 				foreach($files as $file) {
-					if(!(mb_substr($file, -1)=='l')&&!(mb_substr($file, -1)=='.')&&!str_contains($file, 'logo')&&!str_contains($file, 'favicon'))
+					if(!(substr($file, -1)=='l')&&!(substr($file, -1)=='.')&&!str_contains($file, 'logo')&&!str_contains($file, 'favicon'))
 				  echo( "
 				  <div class=\"col-md-4 col-sm-6 col-xs-12\">
 
